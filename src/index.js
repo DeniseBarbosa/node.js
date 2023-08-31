fa8bb925423ea13370a26e9ef6f9a28392f0ac33
Denise Pereira Barbosa <deniseluvoir@gmail.com>
2023-08-31 09:22:37 -0300

    Criando a primeira rota da API

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-// Passo 1: Importar o módulo 'http' do Node.js
+
 const http = require('http');
+const usuarios = require('./mocks/usuarios'); 
 
-// Passo 2: Criar um servidor
-// O método createServer() cria um servidor que vai lidar com as solicitações dos clientes.
-// Ele recebe uma função de callback que é executada sempre que uma solicitação é recebida.
 const server = http.createServer((request, response) => {
-  // Passo 3: Configurar a resposta
-  // Definir o status da resposta como 200 (OK) e o tipo de conteúdo como 'text/html'.
-  response.writeHead(200, {'Content-Type': 'text/html'});
 
-  // Passo 4: Enviar uma resposta para o cliente
-  // Enviar o conteúdo "<h1>Ola Denise!</h1>" como resposta.
-  response.end('<h1>Ola Denise!</h1>');
+  // criando o primeiro andpoint, rota de API 
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  if(request.url === '/usuarios' && request.method === 'GET'){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    // aqui tem que converter o array em uma string
+    // dentro do end sempre precisa mandar uma string  
+    response.end(JSON.stringify(usuarios));
+  }else{
+    response.writeHead(404, {'Content-Type': 'text/html'}); 
+    response.end(`Cannot ${request.method} ${request.url}`); 
+  }
+
+
+  
 });
 
-// Passo 5: Fazer o servidor ouvir uma porta específica
-// O método listen() permite que o servidor comece a "ouvir" em uma porta específica.
-// Quando isso acontece, a função de callback é executada.
-server.listen(3000, () => {
-  console.log('O servidor iniciado em http://localhost:3000');
-});
+
+server.listen(3000, () => console.log('O servidor iniciado em http://localhost:3000'));
---
+++ src/mocks/usuarios.js
@@ -0,0 +1,5 @@
+module.exports = [
+  {id:1, nome: "Denise"},
+  {id:2, nome: "Pedro"},
+  {id:3, nome: "Leticia"}
+];
